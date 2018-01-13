df1042c37b58ece5b84a02cfb7199b7d053a5515
Dreamiiz <dream10005@gmail.com>
2018-01-13 15:49:27 +0700

    Edit hashtag.js

--- hashtag.js
+++ hashtag.js
@@ -4,26 +4,25 @@ let scrape = async () => {
     const browser = await puppeteer.launch({headless: false,timeout: 0});
     const page = await browser.newPage();
 
-    await page.goto('https://www.facebook.com/');    
+    await page.goto('https://www.facebook.com/MTlikesara/');    
    	//await page.goto('https://github.com/search?utf8=%E2%9C%93&q=john&type=');
 
    	const LENGTH_SELECTOR_CLASS = 'ssr-search-result';
    	const LIST_HOTEL_NAME_SELECTOR = '#reactPropertyList > ol > li:nth-child(INDEX) > a > div > section > div.hotel-info > ul > li:nth-child(1) > h3';
-   	const LIST_HOTEL_NAME_SELECTORs = '#reactPropertyList > ol > li:nth-child(INDEX) > a > div.LazyLoad';
-   	
+   	const LIST_HOTEL_NAME_SELECTORs = '#id_5a59c4ec554f56c25729499 > p > a > span > span._58cm';
    	//const a = '#js-pjax-container > div > div.columns > div.column.three-fourths.codesearch-results > div > ul > div:nth-child(INDEX) > div.col-8.pr-3 > h3 > a';
    // 	let listLength = await page.evaluate((sel) => {
    //  	return document.getElementsByClassName(sel).length;
   	// }, LENGTH_SELECTOR_CLASS);
 
   	for (let i = 1; i <= 20; i++) {
-  		let usernameSelector = LIST_HOTEL_NAME_SELECTORs.replace("INDEX", i);
-  		console.log(usernameSelector);
+  		//let usernameSelector = LIST_HOTEL_NAME_SELECTORs.replace("INDEX", i);
+  		//console.log(usernameSelector);
 
     	let email = await page.evaluate((sel) => {
         	let element = document.querySelector(sel);
         	return element? element.innerHTML: null;
-      		}, usernameSelector);
+      		}, LIST_HOTEL_NAME_SELECTORs);
     	console.log(email);
   	}
     //     let loop = 0;
